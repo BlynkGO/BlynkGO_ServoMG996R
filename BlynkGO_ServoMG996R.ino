0334ab462078559bbe71efaf5463b10658ba5c83
BlynkGO <65327357+BlynkGO@users.noreply.github.com>
2022-04-22 18:54:18 +0700

    Update BlynkGO_ServoMG996R.ino

--- BlynkGO_ServoMG996R.ino
+++ BlynkGO_ServoMG996R.ino
@@ -1,10 +1,11 @@
 #include <BlynkGOv2.h>
-#include <ESP32Servo.h>
+#include <ESP32Servo.h>  // ติดตั้งไลบรารี่จาก https://github.com/ShellAddicted/ESP32Servo
 
 #define SERVO_PIN  22
 Servo  servo; //ประกาศตัวแปรแทน Servo
 
 GSlider slider;
+GLabel  label;
 
 void setup() {
   Serial.begin(115200); Serial.println();
@@ -15,11 +16,16 @@ void setup() {
   slider.color(TFT_RED);
   slider.onValueChanged([](GWidget*widget){
     //ดึงค่า slider มากำหนดองศาให้ servo
-    servo.write(slider.value());  
+    label = slider.value();
+    servo.write(slider.value());
   });
-  
+  label.font(prasanmit_40);
+  label.align(slider, ALIGN_TOP,0,-20);
+  label = 0;
 }
 
 void loop() {
   BlynkGO.update();
 }
+
+

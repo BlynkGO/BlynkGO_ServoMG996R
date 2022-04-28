47a7877a628b44d17dda0b5b94076d3dc8327664
BlynkGO <blynkgo@gmail.com>
2022-04-28 17:31:44 +0700

    Update BlynkGO_ServoMG996R.ino

--- BlynkGO_ServoMG996R.ino
+++ BlynkGO_ServoMG996R.ino
@@ -15,8 +15,23 @@ void setup() {
 
   servo.attach(SERVO_PIN, SERVO_CHANNEL);
 
+  slider.size(360,24);
   slider.range(0,180);
   slider.color(TFT_RED);
+  slider.align(ALIGN_BOTTOM,0,-40);
+
+  // ปรับ style ทำการ customize หน้าตาเพิ่มเติมเอง
+  slider.style[KNOB].color(TFT_BLUE, TFT_CYAN);     // สีตัว ปุ่มเลื่อน slider
+  slider.style[KNOB].padding_left_right(20);        // ปรับเพิ่มระยะแนวนอน ของปุ่มเลื่อน slider
+  slider.style[KNOB].padding_top_bottom(15);        // ปรับเพิ่มระยะแนวตั้ง ของปุ่มเลื่อน slider  
+  slider.style[KNOB].corner_radius(20);             // รัศมีมุมโค้งของ ปุ่มเลื่อน slider
+  slider.style[INDIC].padding_top_bottom(5);        // ระยะช่องไฟบนล่าง ระหว่าง พื้นหลัง slider กับ indic
+  slider.style[INDIC].padding_left_right(25);       // ระยะช่องไฟซ้ายขวา ระหว่าง พื้นหลัง slider กับ indic
+  slider.style[BG].opa(30);                         // ปรับความโปร่งใสของ พื้นหลัง slider
+
+  label.font(prasanmit_40);
+  label.align(slider, ALIGN_TOP,0,-20);
+  
   slider.onValueChanged([](GWidget*widget){
     //ดึงค่า slider มากำหนดองศาให้ servo
     label = slider.value();
@@ -25,10 +40,9 @@ void setup() {
       servo.write(slider.value());         // Wait for end of previous rotation.
     });
   });
+  slider = 90;
+  slider.event_send(EVENT_VALUE_CHANGED);           // สั่งให้ slider ทำงานภายใน onValueChanged(..) ที่กำหนดไป
 
-  label.font(prasanmit_40);
-  label.align(slider, ALIGN_TOP,0,-20);
-  label = 0;
 }
 
 void loop() {
